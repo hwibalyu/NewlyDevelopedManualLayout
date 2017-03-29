c9c68db399a7c699648b2bf77191b641eb3edf26
hanee <hwibalyu@naver.com>
2017-03-29 20:12:43 +0900

    Initial commit

---
+++ NewlyDevelopedManualLayout/NewlyDevelopedManualLayout.h
@@ -0,0 +1,19 @@
+//
+//  NewlyDevelopedManualLayout.h
+//  NewlyDevelopedManualLayout
+//
+//  Created by KeonWoo HAN on 2017. 3. 29..
+//  Copyright © 2017년 FunDrinker. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NewlyDevelopedManualLayout.
+FOUNDATION_EXPORT double NewlyDevelopedManualLayoutVersionNumber;
+
+//! Project version string for NewlyDevelopedManualLayout.
+FOUNDATION_EXPORT const unsigned char NewlyDevelopedManualLayoutVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NewlyDevelopedManualLayout/PublicHeader.h>
+
+
